71f952fbe3e4643cea3481e6600657e4f23ec4ab
Timm Knape <timm@kna-st.de>
2017-02-26 09:32:12 +0100

    added localization

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
 var gulp = require('gulp');
 var include = require('gulp-file-include');
+var i18n = require('gulp-i18n-localize');
 var htmlmin = require('gulp-htmlmin');
 var uglify = require('gulp-uglify');
 var minifyCSS = require('gulp-csso');
 var zip = require('gulp-zip');
 
+function intl() {
+    return i18n({
+        locales: ['en-US', 'de-DE'],
+        localeDir: './locales',
+        schema: 'suffix'
+    });
+}
+
 gulp.task('html', function() {
     return gulp.src('*_web.html')
         .pipe(include())
+        .pipe(intl())
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest('dist'));
 });
@@ -15,6 +25,7 @@ gulp.task('html', function() {
 gulp.task('fragment', function() {
     return gulp.src('*_fragment.html')
         .pipe(include())
+        .pipe(intl())
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest('dist'));
 });
